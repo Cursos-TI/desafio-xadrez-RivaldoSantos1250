1f04635f05fe53fc0f167a45c15f1e546c96d8e3
Rivaldo <160893472+RivaldoSantos1250@users.noreply.github.com>
2025-11-21 15:04:35 +0000

    Nivel-Novato

--- xadrez.c
+++ xadrez.c
@@ -4,19 +4,29 @@ int main() {
     int numb = 1;
 
     // Movimentação da Torre
+    printf("Torre\n");
     while (numb <= 5) {
         numb++;
         printf("Cima\n");
     }
 
-    printf("+========================+\n");
+    
 
     // Movimentação do Bispo
+    printf("Bispo\n");
     numb = 1;
     do {
         printf("Cima, Direita\n");
         numb++;
     } while (numb <= 5);
+
+    // Movimentação da Rainha
+    printf("Rainha\n");
+    for (int i = 0; i < 8; i++)
+    {
+         printf("Esquerda\n");
+    }
+    
 
     return 0;
 }
