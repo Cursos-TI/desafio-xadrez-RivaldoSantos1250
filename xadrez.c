40db248e8e5a8b714c6d4336b1abc2b02c5c96ea
Rivaldo <160893472+RivaldoSantos1250@users.noreply.github.com>
2025-11-21 12:29:28 +0000

    Movimentação Torre/Bispo

--- xadrez.c
+++ xadrez.c
@@ -4,8 +4,7 @@ int main() {
     int numb = 1;
 
     // Movimentação da Torre
-    while (numb <= 5)
-    {
+    while (numb <= 5) {
         numb++;
         printf("Cima\n");
     }
@@ -14,12 +13,10 @@ int main() {
 
     // Movimentação do Bispo
     numb = 1;
-    do
-    {
+    do {
         printf("Cima, Direita\n");
         numb++;
     } while (numb <= 5);
-    
 
     return 0;
 }
