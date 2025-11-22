45408600260b7cd748fd898f53bf6ffa33f8bff5
Rivaldo <160893472+RivaldoSantos1250@users.noreply.github.com>
2025-11-22 21:27:12 +0000

    Nivel-mestre

--- xadrez.c
+++ xadrez.c
@@ -1,48 +1,61 @@
 #include <stdio.h>
 
+// Resursividade para da torre.
 void moverTorre(int movTorre) {
-    if(movTorre > 0) {
+    if (movTorre > 0) {
         printf("Cima\n");
-        moverTorre(movTorre -1);     
-    }    
+        moverTorre(movTorre - 1);
+    }
+}
+
+// Resursividade para o bispo com loops aninhados.
+void moverBispo(int movBispo) {
+    if (movBispo > 0) {
+        printf("Cima\n");
+
+        for (int i = 0; i < 3; i++) {
+            if (i == i) { 
+                printf("Direita\n");
+                break;
+            }
+        }
+
+        moverBispo(movBispo - 1);
+    }
+}
+
+// Resursividade para a rainha.
+void moverRainha(int movRainha) {
+    if (movRainha > 0) {
+        printf("Esquerda\n");
+        moverRainha(movRainha - 1);
+    }
 }
 
 int main() {
 
-    int numb = 5;
+    int torre = 3;
 
     // Movimentação da Torre
-    printf("Torre\n");
-    printf("\n");
-    moverTorre(numb);
-
+    printf("Torre:\n");
+    moverTorre(torre);
     printf("\n");
 
     // Movimentação do Bispo
-    printf("Bispo\n");
-    printf("\n");
-    numb = 1;
-    do {
-        printf("Cima, Direita\n");
-        numb++;
-    } while (numb <= 5);
-
+    int bispo = 5;
+    printf("Bispo:\n");
+    moverBispo(bispo);
     printf("\n");
 
     // Movimentação da Rainha
-    printf("Rainha\n");
-    printf("\n");
-    for (int i = 0; i < 8; i++) {
-        printf("Esquerda\n");
-    }
-
+    int rainha = 8;
+    printf("Rainha:\n");
+    moverRainha(rainha);
     printf("\n");
 
     // Movimentação do Cavalo
-    printf("Cavalo\n");
-    printf("\n");
-
     int j;
+    printf("Cavalo:\n");
     do {
         for (j = 0; j < 2; j++) {
             printf("Cima\n");
